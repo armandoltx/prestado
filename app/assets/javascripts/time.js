b52af995b781cf367f2b1ad9bb94d672da54e25c
armandoltx <armando@armandoltx.com>
2016-08-23 07:35:44 +1000

    adding style to home show product and edit user

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,11 +1,22 @@
 //jQuery is required to run this code
 $( document ).ready(function() {
 
+  //time picker
+    $('.datepicker').datetimepicker({
+      inline: true,
+      format:'d.m.Y h:i a',
+      step: 30
+    });
+    console.log('before triggers');
 
-//time picker
-  $('.datepicker').datetimepicker({
-   format:'d.m.Y h:i a',
-    step: 30
-  });
+    // for the timepicker to show when loads the page
+    $('.xdsoft_today_button').click(function(){
+      $(this).datetimepicker('show');
+    });
+    $('.xdsoft_today_button')[0].click();
+
+
+
+    console.log('after triggers');
 
 });
